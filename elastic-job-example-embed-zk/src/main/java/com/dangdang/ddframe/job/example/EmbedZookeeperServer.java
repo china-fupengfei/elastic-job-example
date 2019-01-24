97ec7c876f0cf18236da5dff2351244925af4335
付鹏飞 <fupengfei1@sf-express.com>
2019-01-24 16:56:39 +0800

    cmt

--- elastic-job-example-embed-zk/src/main/java/com/dangdang/ddframe/job/example/EmbedZookeeperServer.java
+++ elastic-job-example-embed-zk/src/main/java/com/dangdang/ddframe/job/example/EmbedZookeeperServer.java
@@ -24,39 +24,42 @@ import java.io.IOException;
 
 /**
  * 内存版的内嵌Zookeeper.
- * 
+ *
  * <p>
- *     仅用于运行Elastic-Job的例子时无需额外启动Zookeeper. 如有必要, 请使用本地环境可用的Zookeeper代替.
+ * 仅用于运行Elastic-Job的例子时无需额外启动Zookeeper. 如有必要, 请使用本地环境可用的Zookeeper代替.
  * </p>
+ *
+ * https://blog.csdn.net/zhu_tianwei/article/details/42047499
+ *
  */
 public final class EmbedZookeeperServer {
-    
+
     private static TestingServer testingServer;
-    
+
     /**
      * 内存版的内嵌Zookeeper.
-     * 
+     *
      * @param port Zookeeper的通信端口号
      */
     public static void start(final int port) {
         try {
-            testingServer = new TestingServer(port, new File(String.format("target/test_zk_data/%s/", System.nanoTime())));
-        // CHECKSTYLE:OFF
+            testingServer = new TestingServer(
+                port, new File(String.format("target/test_zk_data/%s/", System.nanoTime()))
+            );
+            // testingServer.start(); already start at new instance
+            // CHECKSTYLE:OFF
         } catch (final Exception ex) {
-        // CHECKSTYLE:ON
+            // CHECKSTYLE:ON
             ex.printStackTrace();
         } finally {
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(1000L);
-                        testingServer.close();
-                    } catch (final InterruptedException | IOException ex) {
-                    }
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    Thread.sleep(1000L);
+                    testingServer.close();
+                } catch (final InterruptedException | IOException ex) {
+                    ex.printStackTrace();
                 }
-            });
+            }));
         }
     }
 }
